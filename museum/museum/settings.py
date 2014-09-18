f1e5648847f9fb3fcdb8594bb3002dcc6cbdafc3
tab262 <caseface31@gmail.com>
2014-09-18 13:37:39 -0400

    modified setting file

--- museum/museum/settings.py
+++ museum/museum/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y0&!ee9s&_j**^8^(%cb_$o5!-jk+v-w6i%@y$9pndz1njgit+'
+SECRET_KEY = open('secret.txt',r').read().replace("\n",'') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
